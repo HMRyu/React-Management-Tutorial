de8bf7f2696134b21203b71e0d47f6ce164b9346
HMRyu <coc1381@naver.com>
2021-06-20 20:37:38 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer'
 import { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/styles';
+
+const styles = theme => ({
+  root : {
+    width: '100%',
+    overflowX : "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [{
   'id' : 1,
@@ -31,9 +47,21 @@ const customers = [{
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-      {
+      <Paper className={classes.root}> 
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+        {
         customers.map(c => {
           return (
             <Customer
@@ -48,9 +76,11 @@ class App extends Component {
           );
         })
       }
-      </div>
+      </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
